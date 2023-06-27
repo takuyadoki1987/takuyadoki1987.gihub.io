50cc1e892c0473a244683a601fdb7422d544e8cc
takuyadoki1987 <132044078+takuyadoki1987@users.noreply.github.com>
2023-06-27 23:44:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
-let y = canvas.height - 30;
+let y = canvas.height - 10;
 let dx = 2;
 let dy = -2;
-const ballRadius = 5;
+const ballRadius = 3;
 const paddleHeight = 5;
 const paddleWidth = 50;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+let spacePressed = false;
 const brickRowCount = 3;
 const brickColumnCount = 5;
 const brickWidth = 45;
-const brickHeight = 13;
+const brickHeight = 5;
 const brickPadding = 2;
 const brickOffsetTop = 20;
 const brickOffsetLeft = 35;
@@ -27,7 +28,6 @@ for (var c = 0; c < brickColumnCount; c++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
-
 //キーボード操作のイベントリスナー
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
@@ -58,7 +58,7 @@ document.addEventListener("mousemove", mouseMoveHandler)
 
 // ランダムに色を作る
 function randomColor() {
-    let color = {r:0, g:0, b:0};
+    let color = { r: 0, g: 0, b: 0 };
     for (let i in color) {
         color[i] = Math.floor(Math.random() * 256);
     }
@@ -70,7 +70,7 @@ function randomColor() {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "#ffffff";
     // ctx.fillStyle = randomColor();
     ctx.fill();
     ctx.closePath();
@@ -79,14 +79,15 @@ function drawBall() {
 // キャンバスにパドルを描く
 function drawPaddle() {
     ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
+    ctx.rect(paddleX, canvas.height - 15, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#ffffff";
     ctx.fill();
     ctx.closePath();
 }
 
 // キャンバスにブロックを描く
 function drawBricks() {
+    const color = ["#F39800", "#FFF100", "#8FC31F"]
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status === 1) {
@@ -96,7 +97,7 @@ function drawBricks() {
                 bricks[c][r].y = brickY;
                 ctx.beginPath();
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "brown";
+                ctx.fillStyle = color[r];
                 ctx.fill();
                 ctx.closePath();
             }
@@ -104,15 +105,30 @@ function drawBricks() {
     }
 }
 
+// ゲーム開始前のテキストを表示
+function drawStartText() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.textAlign = "center"
+    ctx.fillText("スペースを押してゲームスタート", canvas.width / 2, canvas.height / 2);
+}
+
 // 実際に関数を呼び出してキャンバスに描く
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (!spacePressed) {
+        drawStartText();
+        return;
+    }
+
     drawBricks();
     drawBall();
     drawPaddle();
     collisionDetection();
     drawScore();
     drawLives();
+
     x += dx;
     y += dy;
     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
@@ -122,21 +138,22 @@ function draw() {
     // ボールがパドルに当たったか判定する
     if (y + dy < ballRadius) {
         dy = - dy;
-
-        // ctx.fillStyle = randomColor();
-    } else if (y + dy > canvas.height - ballRadius) {
+    } else if (y + dy > canvas.height -10 - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         } else {
             lives--;
             if (lives === 0) {
+                // ctx.fillStyle = "black";
+                // ctx.font = "32px Noto Sans JP";
+                // ctx.fillText("ゲームオーバー", canvas.width /2, canvas.height /2);
                 alert("ゲームオーバー");
                 document.location.reload();
             } else {
                 x = canvas.width / 2;
                 y = canvas.height - 30;
                 dx = 2;
-                dy =-2;
+                dy = -2;
                 paddleX = (canvas.width - paddleWidth) / 2;
             }
         }
@@ -155,6 +172,14 @@ function keyDownHandler(e) {
         rightPressed = true;
     } else if (e.key === "Left" || e.key === "ArrowLeft") {
         leftPressed = true;
+    } else if (e.key === " ") {
+        if (!spacePressed) {
+            spacePressed = true;
+            draw();
+        }
+        // e.preventDefault();
+        // console.log(spacePressed);
+
     }
 }
 
@@ -169,10 +194,10 @@ function keyUpHandler(e) {
 
 // マウス操作でパドルを動かす
 function mouseMoveHandler(e) {
-    const relativeX = e.clientX - canvas.width;
-    console.log(relativeX);
+    const relativeX = e.clientX - canvas.getBoundingClientRect().left//window.innerWidth / 2;
+    // console.log(relativeX);
     if (relativeX - paddleWidth / 2 > 0 && relativeX + paddleWidth / 2 < canvas.width) {
-        paddleX = relativeX - paddleWidth /2;
+        paddleX = relativeX - paddleWidth / 2;
     }
 }
 
@@ -199,18 +224,18 @@ function collisionDetection() {
 
 //キャンバスにスコアを表示する
 function drawScore() {
-    ctx.font = "12px Arial";
-    ctx.fillStyle = "red";
+    ctx.font = "10px Arial";
+    ctx.fillStyle = "white";
     ctx.fillText(`スコア: ${score}`, 8, 15);
 }
 
 // ライフをキャンバスに表示する
 function drawLives() {
-    ctx.font = "12px Arial";
-    ctx.fillStyle = "red";
-    ctx.fillText(`ライフ: ${lives}`, canvas.width -50, 15);
+    ctx.font = "10px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(`ライフ: ${lives}`, canvas.width - 50, 15);
 }
 
 // draw関数を10msごとに実行する
-draw();
+// draw();
 // clearInterval(interval);
