33efb570a0788aacfc74e55dbc79b020c77fdbe0
takuyadoki1987 <132044078+takuyadoki1987@users.noreply.github.com>
2023-07-02 23:01:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-let x = canvas.width / 2;
-let y = canvas.height - 10;
 let dx = 2;
 let dy = -2;
 const ballRadius = 3;
 const paddleHeight = 5;
 const paddleWidth = 50;
 let paddleX = (canvas.width - paddleWidth) / 2;
+let x = canvas.width / 2;
+let y = canvas.height - 18;
 let rightPressed = false;
 let leftPressed = false;
 let spacePressed = false;
-const brickRowCount = 3;
+const brickRowCount = 5;
 const brickColumnCount = 5;
 const brickWidth = 45;
 const brickHeight = 5;
@@ -21,6 +21,7 @@ const brickOffsetLeft = 35;
 let score = 0;
 let lives = 3;
 
+//空の二次元配列を作成
 const bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -35,59 +36,31 @@ document.addEventListener("keyup", keyUpHandler);
 //マウス操作のイベントリスナー
 document.addEventListener("mousemove", mouseMoveHandler)
 
-//赤く塗りつぶされた四角を描画
-// ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "red";
-// ctx.fill();
-// ctx.closePath();
-
-//緑に塗りつぶされた丸を描画
-// ctx.beginPath();
-// ctx.arc(240, 60, 20,0, Math.PI * 2);
-// ctx.fillStyle = "green";
-// ctx.fill();
-// ctx.closePath();
-
-//青い枠の四角を描画
-// ctx.beginPath();
-// ctx.rect(160, 10, 100, 100);
-// ctx.strokeStyle = "blue";
-// ctx.stroke();
-// ctx.closePath();
-
-// ランダムに色を作る
-function randomColor() {
-    let color = { r: 0, g: 0, b: 0 };
-    for (let i in color) {
-        color[i] = Math.floor(Math.random() * 256);
-    }
-    let myColor = "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
-    return myColor;
-}
+// ランダムに色を作る 16進数ver
+// function randomColor() {
+//     return Math.random().toString(16).slice(-6);
+// }
 
 // canvasにボールを描く
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#ffffff";
-    // ctx.fillStyle = randomColor();
     ctx.fill();
     ctx.closePath();
 }
-
 // キャンバスにパドルを描く
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - 15, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#ffffff";
+    ctx.fillStyle = "#0066FF";
     ctx.fill();
     ctx.closePath();
 }
 
 // キャンバスにブロックを描く
 function drawBricks() {
-    const color = ["#F39800", "#FFF100", "#8FC31F"]
+    const color = ["#1E98B9", "#3EBA2B", "#FFEA2A", "#D04255", "#8858AA"]
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status === 1) {
@@ -105,22 +78,10 @@ function drawBricks() {
     }
 }
 
-// ゲーム開始前のテキストを表示
-function drawStartText() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "#0095DD";
-    ctx.textAlign = "center"
-    ctx.fillText("スペースを押してゲームスタート", canvas.width / 2, canvas.height / 2);
-}
 
 // 実際に関数を呼び出してキャンバスに描く
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    if (!spacePressed) {
-        drawStartText();
-        return;
-    }
 
     drawBricks();
     drawBall();
@@ -129,40 +90,51 @@ function draw() {
     drawScore();
     drawLives();
 
+    if (!spacePressed) {
+        return;
+    }
+
+
+    //ボールと横壁の衝突判定
     x += dx;
     y += dy;
     if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
-        // ctx.fillStyle = randomColor();
     }
-    // ボールがパドルに当たったか判定する
+    // ボールと上の壁の衝突判定
     if (y + dy < ballRadius) {
         dy = - dy;
-    } else if (y + dy > canvas.height -10 - ballRadius) {
+        //ボールが下壁に当たったらライフが減る
+    } else if (y + dy > canvas.height - 15) {
+        //ボールがパドルの幅の範囲にあれば跳ね返す
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
+            //そうでなければライフをマイナス１する
         } else {
             lives--;
+            spacePressed = false;
+            //ライフが0になったらゲームオーバーを表示する
             if (lives === 0) {
-                // ctx.fillStyle = "black";
-                // ctx.font = "32px Noto Sans JP";
-                // ctx.fillText("ゲームオーバー", canvas.width /2, canvas.height /2);
-                alert("ゲームオーバー");
-                document.location.reload();
+                gameOver();
+                return;
+                //そうでなければパドルの位置とボールの位置、移動量を初期値に戻す
             } else {
                 x = canvas.width / 2;
-                y = canvas.height - 30;
+                y = canvas.height - 18;
                 dx = 2;
                 dy = -2;
                 paddleX = (canvas.width - paddleWidth) / 2;
             }
         }
     }
+    //右キーが押されればパドルのX座標をプラス3する
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
         paddleX += 3
+    //左キーが押されればパドルのX座標をマイナス３する
     } else if (leftPressed && paddleX > 0) {
         paddleX -= 3;
     }
+    gameReload();
     requestAnimationFrame(draw);
 }
 
@@ -177,9 +149,6 @@ function keyDownHandler(e) {
             spacePressed = true;
             draw();
         }
-        // e.preventDefault();
-        // console.log(spacePressed);
-
     }
 }
 
@@ -200,7 +169,6 @@ function mouseMoveHandler(e) {
     }
 }
 
-
 // ボールとブロックの衝突検知
 function collisionDetection() {
     for (let c = 0; c < brickColumnCount; c++) {
@@ -209,12 +177,23 @@ function collisionDetection() {
             if (b.status === 1) {
                 if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                     dy = -dy;
+                    // ブロックを消すたびにボールの速度を上げる
+                    if (dy > 0) {
+                        dy += 0.1;
+                    } else {
+                        dy -= 0.1;
+                    }
+                    if (dx > 0) {
+                        dx += 0.1;
+                    } else {
+                        dx -= 0.1;
+                    }
+
                     b.status = 0;
                     score++;
                     if (score === brickRowCount * brickColumnCount) {
-                        alert("ゲームクリア！");
-                        document.location.reload();
-                        clearInterval(interval);
+                        gameClear();
+                        return;
                     }
                 }
             }
@@ -224,18 +203,61 @@ function collisionDetection() {
 
 //キャンバスにスコアを表示する
 function drawScore() {
-    ctx.font = "10px Arial";
+    ctx.font = "10px Noto Sans JP";
     ctx.fillStyle = "white";
+    ctx.textAlign = "left";
     ctx.fillText(`スコア: ${score}`, 8, 15);
 }
 
 // ライフをキャンバスに表示する
 function drawLives() {
-    ctx.font = "10px Arial";
+    ctx.font = "10px Noto Sans JP";
     ctx.fillStyle = "white";
-    ctx.fillText(`ライフ: ${lives}`, canvas.width - 50, 15);
+    ctx.textAlign = "center";
+    if (lives >= 0) {
+        ctx.fillText("ライフ：" + "❤".repeat(lives), canvas.width - 50, 15);
+    } else {
+        document.location.reload();
+    }
 }
+
+// ゲーム開始前のテキストを表示
+function drawStartText() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "white ";
+    ctx.textAlign = "center"
+    ctx.fillText("press the space key", canvas.width / 2, canvas.height / 2);
+}
+
+// ゲームオーバー時のテキスト表示
+function gameOver() {
+    ctx.font = "24px Noto Sans JP";
+    ctx.fillStyle = "red";
+    // ctx.textAlign = "left";
+    ctx.fillText("GAME OVER", canvas.width / 2, canvas.height / 2);
+    spacePressed = false;
+}
+
+//ゲームクリア時のテキスト表示
+function gameClear() {
+    ctx.font = "24px Noto Sans JP";
+    ctx.fillStyle = "yellow";
+    ctx.fillText("CONGRATULATIONS!!", canvas.width / 2, canvas.height / 2);
+    spacePressed = false;
+}
+
+// ゲームクリア後初期状態に戻す
+console.log(spacePressed)
+function gameReload() {
+    if  (score === brickRowCount * brickColumnCount && spacePressed === true) {
+        document.location.reload();
+        clearInterval(interval);
+    }
+}
+
 drawBricks();
 drawBall();
 drawPaddle();
-
+drawScore();
+drawLives();
+drawStartText();
